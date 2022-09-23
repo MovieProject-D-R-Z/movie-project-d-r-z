326ba4b65423a03645c2afbb4a8a279c601fdfbb
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-09-23 10:33:52 -0500

    deleted jalopy img assets. added movies route. created get all and getbyID in movies controller

---
+++ src/main/java/projectrzd/magicproject/MoviesSeeder.java
@@ -0,0 +1,16 @@
+package projectrzd.magicproject;
+
+import projectrzd.magicproject.data.Genre;
+import projectrzd.magicproject.data.Movie;
+
+import java.util.List;
+//
+//public class MoviesSeeder {
+//
+//    Genre crime = new Genre(1, "Crime", null);
+//
+//
+//    public List<Movie> moviesSeeder(){
+//        new Movie(1,"The Godfather", "Francis Ford Coppola", "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.", "", 1 )
+//    }
+//}
---
+++ src/main/resources/js/views/Movies.js
@@ -0,0 +1,14 @@
+export default function Movies(props) {
+
+    return `
+        <body>
+            <h1 id="homeViewH1">Greatest movies EVER! At the tip of your hands.</h1>
+            <div id="homeContainer">
+                Movie View
+            </div>
+        </body>
+    `;
+}
+
+export function MovieEvents() {
+}
