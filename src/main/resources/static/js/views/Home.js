b7ac6bf2cbf805e7cd39bc026840f0c263ba21ac
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-09-25 22:40:48 -0500

    working on horizontal scroll

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -1,36 +1,37 @@
 const BASE_URI = `${BACKEND_HOST}/api/s3/download`;
 
-
+let data;
 export default function Home(props) {
+    data=props.TmbdMovies;
 
     return `
         <body>
             <h1 id="homeViewH1">Greatest movies EVER! At the tip of your hands.</h1>
-            <div id="homeContainer">
-                ${makeCards(props)}
-            </div>
+            <div id="moviesHere" class = "scrolling-wrapper"></div>
         </body>
     `;
 }
 
 function makeCards(props) {
-    let htmlString = '';
+    // let htmlString = '';
+    // data=props.TmbdMovies;
+    let placeMoviesHere = document.querySelector("#moviesHere");
 
-    console.log(props.TmbdMovies.results[0].poster_path);
-    console.log((props.movies[0].title));
+    console.log(data.results[0].poster_path);
+    // console.log((props.movies[0].title));
 
 
-    for (let i = 0; i < props.TmbdMovies.results.length; i++) {
-
-        htmlString  += `<div class="flip-card">
+    for (let i = 0; i < data.results.length; i++) {
+        placeMoviesHere.innerHTML +=
+         `<div class="flip-card">
                                     <div class="flip-card-inner">
                                        <div class="flip-card-front">
-                                         <img id="movieTemplate" src="https://image.tmdb.org/t/p/w300${props.TmbdMovies.results[i].poster_path}" alt="movie picture">
-                                         <p class="movieDetails">${props.TmbdMovies.results[i].original_title}</p>
+                                         <img id="movieTemplate" src="https://image.tmdb.org/t/p/w300${data.results[i].poster_path}" alt="movie picture">
+                                         <p class="movieDetails">${data.results[i].original_title}</p>
                                          <p class="movieDetails"></p>
                                        </div>   
                                        <div class="flip-card-back">
-                                         <p class="backOverview">${props.TmbdMovies.results[i].overview}</p>
+                                         <p class="backOverview">${data.results[i].overview}</p>
                                          <div>
                                          <a href=""><i class="fa-solid fa-circle-play"></i></a>
                                          <a href=""><i class="fa-solid fa-circle-plus"></i></a>
@@ -38,12 +39,13 @@ function makeCards(props) {
                                         </div>
                                        </div>
                                     </div>         
-                                 </div>
-                                 <hr>`
+          </div>
+                                 `
     }
 
-    return htmlString;
+    // return htmlString;
 }
 
 export function HomeEvents() {
+    makeCards();
 }
--- src/main/resources/static/js/views/Movies.js
+++ src/main/resources/static/js/views/Movies.js
@@ -3,9 +3,9 @@ let movies;
 export default function MoviesIndex(props) {
     console.log(props);
     movies=props.movies
-    console.log(movies[0].title);
+    alert(movies[0].title);
 
-    const postsHTML = generatePostsHTML(props.posts);
+    const moviesHTML = generateMoviesHTML(movies);
     return `     
         <h1 style="color: white">Movies</h1>
     `;
